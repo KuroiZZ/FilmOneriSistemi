b879e571819989f9c38da52dab8bf868d9533c3c
Habil Tatarogullari <habiltatar01@gmail.com>
2024-10-22 15:13:16 +0300

    film normalize fonksiyonu geliştirildi.

--- film.py
+++ film.py
@@ -2,19 +2,22 @@ import pandas as pd
 
 movies = pd.read_csv('film_veri/movie.csv')
 Kategoriler = ["Children", "Animation", "Fantasy", "War", "Horror", "Thriller", "Mystery", "Crime", "Sci-Fi", "Musical"]
+movies_normalized = pd.DataFrame(columns=movies.columns)
 
-i = 0
+movieCount = 0
+selectedMovieCount = 0
 for Fkategoriler in movies["genres"]:
     Sahip = False
     for kategori in Kategoriler:
         if(kategori in Fkategoriler):
             Sahip = True
             break
-    if(not Sahip):
-        movies = movies.drop(i)
-    i += 1
+    if(Sahip):
+        movies_normalized.loc[selectedMovieCount] = movies.iloc[movieCount]
+        selectedMovieCount +=1
+    movieCount += 1
 
-movies.to_csv("film_veri_normalized/movies_normalized.csv", index=False)
+movies_normalized.to_csv("film_veri_normalized/movies_normalized.csv", index=False)
 
 
 
