69fd644a00ce1a3f6a01b615d3ab78c83fc949be
Habil Tatarogullari <habiltatar01@gmail.com>
2024-10-28 16:28:12 +0300

    dosyaya yazdırma işlemi parametre ile yapılmaya başlandı. tek tırnaklar çift tırnak ile değişti.

--- film.py
+++ film.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-def NormalizeMovies():
-    movies = pd.read_csv('film_veri/movie.csv')
+def NormalizeMovies(writeFile):
+    movies = pd.read_csv("film_veri/movie.csv")
 
     Kategoriler = ["Children", "Animation", "Fantasy", "War", "Horror", "Thriller", "Mystery", "Crime", "Sci-Fi", "Musical"]
     movies_filtered = movies[movies["genres"].str.contains('|'.join(Kategoriler))]
 
-    #movies_filtered.to_csv("film_veri_normalized/movies_normalized.csv", index=False)
+    if(writeFile):
+        movies_filtered.to_csv("film_veri_normalized/movies_normalized.csv", index=False)
 
-def NormalizeViews():
+def NormalizeViews(writeFile):
     rating = pd.read_csv("film_veri/rating.csv", usecols=["userId", "movieId"])
 
     movies = pd.read_csv("film_veri_normalized/movies_normalized.csv", usecols=["movieId"])
@@ -17,15 +18,19 @@ def NormalizeViews():
 
     rating_filtered = rating[rating["movieId"].isin(list(movieList))]
 
-    #rating_filtered.to_csv("film_veri_normalized/views_normalized.csv", index=False)
+    if(writeFile):
+        rating_filtered.to_csv("film_veri_normalized/views_normalized.csv", index=False)
 
 
-def NormalizeUsers():
+def NormalizeUsers(writeFile):
     rating = pd.read_csv("film_veri_normalized/views_normalized.csv")
 
-    userWithMovies = rating.groupby(["userId"], as_index=False).agg({'movieId' : lambda x: '|'.join(map(str, x))})
+    userWithMovies = rating.groupby(["userId"], as_index=False).agg({"movieId" : list})
 
-    #userWithMovies.to_csv("film_veri_normalized/user_normalized.csv", index=False)
+    if(writeFile):
+        userWithMovies.to_csv("film_veri_normalized/user_normalized.csv", index=False)
+
+
     
 
 
