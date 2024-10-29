ab370b9c26740a16ea7b01f34db53f6d05760714
Habil Tatarogullari <habiltatar01@gmail.com>
2024-10-29 11:57:40 +0300

    ReadRules fonksiyonu SuggestFilms ile değiştirildi. Öneriler ismiyle birlikte bir liste olarak döndürülmeye başlandı. FindPopularFilms fonksiyonu eklendi.

--- film.py
+++ film.py
@@ -33,6 +33,23 @@ def NormalizeUsers(writeFile):
         userWithMovies.to_csv("film_veri_normalized/user_normalized.csv", index=False)
 
 
+def FindPopularFilms(writeFile):
+    Kategoriler = ["Children", "Animation", "Fantasy", "War", "Horror", "Thriller", "Mystery", "Crime", "Sci-Fi", "Musical"]
+    views = pd.read_csv("film_veri_normalized/views_normalized.csv")
+    movies = pd.read_csv("film_veri_normalized/movies_normalized.csv")
+
+    popularMoviesDF = pd.DataFrame(columns=["category", "movies"])
+
+    for kategori in Kategoriler:
+        moviesNew = movies[movies['genres'].str.contains(kategori)]["movieId"].values.tolist()
+        filteredViews = views[views["movieId"].isin(moviesNew)]
+        popularMovies = filteredViews["movieId"].value_counts().head(50).index.tolist()
+        popularMoviesDF.loc[len(popularMoviesDF)] = [kategori, popularMovies]
+
+    if(writeFile):
+        popularMoviesDF.to_csv("film_veri_normalized/popular_movies.csv", index=False)
+
+
 def CreateMatris(writeFile):
 
     userWmovies = pd.read_csv("film_veri_normalized/user_normalized.csv")
@@ -66,18 +83,26 @@ def frozenset_string_to_list(frozenset_str):
     return list(map(int, items))
 
 
-def ReadRules(movieId):
+def SuggestFilms(movieId):
     rules = pd.read_csv("Rules/rules.csv")
+    movies = pd.read_csv("film_veri_normalized/movies_normalized.csv")
 
     rules["antecedents"] = rules["antecedents"].apply(frozenset_string_to_list)
     rules["consequents"] = rules["consequents"].apply(frozenset_string_to_list)
     
-
     filtered_rules = rules[rules["antecedents"].apply(lambda x: x == movieId)]
     filtered_rules = filtered_rules.sort_values(by="lift", ascending=False)
     filtered_rules["consequents_len"] = filtered_rules["consequents"].apply(lambda x: len(x))
 
-    print(filtered_rules[filtered_rules["consequents_len"] == 1].head(20))
+    suggestionIds = sum(filtered_rules[filtered_rules["consequents_len"] >= 1]["consequents"].head(20), [])
+    suggestions = []
+
+    for suggest in suggestionIds:
+        suggestTitle = movies[movies["movieId"] == suggest]["title"].values[0]
+        suggestions.append(suggestTitle)
+
+    return suggestions
+
 
 
     
