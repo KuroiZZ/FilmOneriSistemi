f82f318b6d4171d5e40ef78f52e1458d101e0bf6
Habil Tatarogullari <habiltatar01@gmail.com>
2024-11-02 15:02:59 +0300

    Normalize etme fonksiyonları taşındı.

---
+++ Normalize.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def NormalizeMovies(writeFile):
+    movies = pd.read_csv("film_veri/movie.csv")
+
+    categories = ["Children", "Animation", "Fantasy", "War", "Horror", "Thriller", "Mystery", "Crime", "Sci-Fi", "Musical"]
+
+    movies_filtered = movies[movies["genres"].str.contains('|'.join(categories))]
+
+    if(writeFile):
+        movies_filtered.to_csv("film_veri_normalized/movies_normalized.csv", index=False)
+    else:
+        print(movies_filtered)
+
+def NormalizeViews(writeFile):
+    views = pd.read_csv("film_veri/rating.csv", usecols=["userId", "movieId"])
+
+    movie_list = pd.read_csv("film_veri_normalized/movies_normalized.csv")["movieId"].tolist()
+
+    rating_filtered = views[views["movieId"].isin(list(movie_list))]
+
+    if(writeFile):
+        rating_filtered.to_csv("film_veri_normalized/views_normalized.csv", index=False)
+    else:
+        print(rating_filtered)
+
+def NormalizeUsers(writeFile):
+    views = pd.read_csv("film_veri_normalized/views_normalized.csv")
+
+    user_with_movies = views.groupby(["userId"], as_index=False).agg({"movieId" : list})
+
+    if(writeFile):
+        user_with_movies.to_csv("film_veri_normalized/user_normalized.csv", index=False)
+    else:
+        print(user_with_movies)
+
+
+NormalizeMovies(False)
+NormalizeViews(False)
+NormalizeUsers(False)
